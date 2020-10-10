32dd87fb56ea2be267914e48dbce35c0b4bcc580
Dlarsen93 <dlarsen93@byui.edu>
2020-10-10 14:40:02 -0600

    attempt 8

--- Lesson 4/js/main.js
+++ Lesson 4/js/main.js
@@ -1,7 +1,7 @@
 let hambutton = document.querySelector('#ham');
 let mainnav = document.querySelector('.navigation')
 
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+hambutton.addEventListener('click', function() {mainnav.classList.toggle('responsive')});
 
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
 
