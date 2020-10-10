487fbe69060ab42a13e105f3013b6c8431cb3b1a
Dlarsen93 <dlarsen93@byui.edu>
2020-10-10 14:33:27 -0600

    attempt 6

--- Lesson 4/js/main.js
+++ Lesson 4/js/main.js
@@ -1,3 +1,7 @@
-function toggleMenu() {
-    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
-}
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
