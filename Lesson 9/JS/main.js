95a5dd185df03c91d56028d9bc6480f77f693599
Dlarsen93 <dlarsen93@byui.edu>
2020-11-12 17:28:16 -0700

    prophets

--- Lesson 9/JS/main.js
+++ Lesson 9/JS/main.js
@@ -1,4 +1,3 @@
 document.getElementById('ham').addEventListener('click', function() {document.querySelector('.navigation').classList.toggle('responsive')});
 
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
-
