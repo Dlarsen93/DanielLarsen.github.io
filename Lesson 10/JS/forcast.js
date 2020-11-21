1e92eeb83b0b65798484c1e51bacff968c72ecb6
Dlarsen93 <dlarsen93@byui.edu>
2020-11-21 10:44:27 -0700

    les 10.2

--- Lesson 10/JS/forcast.js
+++ Lesson 10/JS/forcast.js
@@ -1,11 +1,37 @@
-const apiURL = 'api.openweathermap.org/data/2.5/forecast?id=56044&units=imperial&appid=cc2a9728fbac4669b86898fecbccc39b';
+const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=cc2a9728fbac4669b86898fecbccc39b';
 
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject); //be sure to delete this before submitting!
 
-    
+    const forcast = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
+    console.log(forcast);
 
+    const daysOfWeek = ['Sunday', 'Monday', 'Tursday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    for(let i = 0; i < forcast.length; i++){
+      const forecastDate = new Date(forecast[i].dt_txt);
+            let iconSrc = 'https://openweathermap.org/img/w/' + forecast[i].weather[0].icon + '.png';
+            let desc = forecast[i].weather[0].description;
+            let forecastCard = document.createElement('li');
+            let weekday = document.createElement('h3');
+            let forecastIcon = document.createElement('img');
+            let forecastTemp = document.createElement('p');
 
+            //store text/content in the elements
+            weekday.textContent = weekdays[foreDate.getDay()];
+            forecastIcon.setAttribute('src', iconSrc);
+            forecastIcon.setAttribute('alt', desc);
+            forecastIcon.style.width = '4.4em';
+            forecastTemp.innerHTML = forecast[i].main.temp + '&#176;F';
+
+            //append elements to li item
+            forecastCard.appendChild(weekday);
+            forecastCard.appendChild(forecastIcon);
+            forecastCard.appendChild(forecastTemp);
+            forecastCard.classList.add("card");
+
+            document.getElementById('fiveForecast').appendChild(forecastCard);
+
+    }
 });
