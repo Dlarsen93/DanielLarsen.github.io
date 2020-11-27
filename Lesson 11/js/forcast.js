25be85cedd312c47cab1d77512215ec52081d47d
Dlarsen93 <dlarsen93@byui.edu>
2020-11-27 14:22:35 -0700

    lesson 11

---
+++ Lesson 11/js/forcast.js
@@ -0,0 +1,37 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=cc2a9728fbac4669b86898fecbccc39b';
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject); //be sure to delete this before submitting!
+
+    const forcast = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
+    console.log(forcast);
+
+    const daysOfWeek = ['Sunday', 'Monday', 'Tursday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    for(let i = 0; i < forcast.length; i++){
+      const forcastDate = new Date(forcast[i].dt_txt);
+            let iconSrc = 'https://openweathermap.org/img/w/' + forcast[i].weather[0].icon + '.png';
+            let desc = forcast[i].weather[0].description;
+            let forcastCard = document.createElement('li');
+            let weekday = document.createElement('h3');
+            let forcastIcon = document.createElement('img');
+            let forcastTemp = document.createElement('p');
+
+            //store text/content in the elements
+            weekday.textContent = daysOfWeek[forcastDate.getDay()];
+            forcastIcon.setAttribute('src', iconSrc);
+            forcastIcon.setAttribute('alt', desc);
+            forcastIcon.style.width = '4.4em';
+            forcastTemp.innerHTML = forcast[i].main.temp + '&#176;F';
+
+            //append elements to li item
+            forcastCard.appendChild(weekday);
+            forcastCard.appendChild(forcastIcon);
+            forcastCard.appendChild(forcastTemp);
+            forcastCard.classList.add("card");
+
+            document.getElementById('fiveDayForcast').appendChild(forcastCard);
+
+    }
+});
---
+++ Lesson 11/js/home-cards.js
@@ -0,0 +1,53 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject);
+        const towns = jsonObject['towns'];
+        for (let i = 0; i < towns.length; i++) {
+
+            if (towns[i].name == "Preston" || towns[i].name == "Soda Springs" || towns[i].name == "Fish Haven") {
+                
+                let townInfo = document.createElement('section');
+                let textBox = document.createElement('div');
+                let h2 = document.createElement('h2');
+                let motto = document.createElement('h3');
+                let founded = document.createElement('p');
+                let population = document.createElement('p');
+                let rain = document.createElement('p');
+                let image = document.createElement('img');
+
+                //information to populate
+                townInfo.setAttribute('class', towns[i].name);
+                h2.textContent = towns[i].name;
+                motto.textContent = towns[i].motto;
+                founded.textContent = "Year Founded: " + towns[i].yearFounded;
+                population.textContent = "Pop: " + towns[i].currentPopulation;
+                rain.textContent = "Yearly Rain Fall: " + towns[i].averageRainfall;
+                image.setAttribute("src", "images/" + towns[i].photo);
+                image.setAttribute('alt', towns[i].name);
+
+                textBox.append(h2, motto, founded, population, rain);
+                townInfo.append(textBox, image);
+
+                document.querySelector('div.towns').appendChild(townInfo);
+                
+                //Practice
+                /*
+                let card = document.createElement('section');
+                let h2 = document.createElement('h2');
+                let image = document.createElement('img');
+
+                h2.innerHTML = `${towns[i].name} <span class="highlight">${towns[i].name}</span>`;
+                image.setAttribute('src', 'images/', + towns[i].photo);
+
+                card.appendChild(h2);
+                */
+
+                }
+
+        }
+    });
