68e3751c7edf3aac4a0071b77e92757421472c2b
Dlarsen93 <dlarsen93@byui.edu>
2020-11-28 19:44:25 -0700

    11.11

--- Lesson 11/js/soda.js
+++ Lesson 11/js/soda.js
@@ -1,5 +1,32 @@
 const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5607916&units=imperial&appid=cc2a9728fbac4669b86898fecbccc39b';
 
+const summaryapi = "https://api.openweathermap.org/data/2.5/weather?id=5607916&units=imperial&appid=cc2a9728fbac4669b86898fecbccc39b";
+
+fetch(summaryapi)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+        //changes the current info spans with the corresponding values
+        let description = jsObject.weather[0].description;
+        let weatherDesc = description.charAt(0).toUpperCase() + description.slice(1);
+        
+        document.getElementById('current-temp').textContent = Math.round(jsObject.main.temp);
+        document.getElementById('current-desc').textContent = weatherDesc;
+        document.getElementById('high-temp').textContent = Math.round(jsObject.main.temp_max);
+        document.getElementById('current-humidity').textContent = Math.round(jsObject.main.humidity);
+        document.getElementById('windSpeed').textContent = Math.round(jsObject.wind.speed);
+
+        //calculates wind chill
+        function calcWindChill(temperature, speed) {
+            let chill = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temperature * Math.pow(speed, 0.16));
+            return Math.round(chill);
+        }
+
+        var temp = jsObject.main.temp;
+        var windSpeed = jsObject.wind.speed;
+        document.getElementById('windChill').innerHTML = calcWindChill(temp, windSpeed);
+    });
+
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
